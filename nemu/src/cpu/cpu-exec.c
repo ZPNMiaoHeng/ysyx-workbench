2a20d0a0f2522f2583e6dde5973ed7137fbe2212
220031-Miao Heng <1098011272@qq.com>
2023-12-13 03:06:18 +0000

    将监视点功能加入nemuconfig

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,11 +40,11 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+#ifdef CONFIG_WATCHPOINT_COND
   if(watchpoint_checkout() == false) {
     nemu_state.state = NEMU_STOP;
-    // sdb_mainloop();
   }
-  // TODO - 扫描所有的监视点，并对其中表达式求值；若求值结果变化则改变nemu执行状态；
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
