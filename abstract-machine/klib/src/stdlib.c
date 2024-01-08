30ea1cae3b1b201d267984e8e57696107e7c75dd
220031-Miao Heng <1098011272@qq.com>
2024-01-08 14:25:26 +0000

    malloc还是存在bug

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -68,14 +68,32 @@ void itoa(const int n, char *buf) {
   mini_itoa(n, 10, 1, 0, buf);
 }
 
+static char *hbrk;
+static void malloc_reset() {
+  hbrk = (void *)ROUNDUP(heap.start, 8);
+}
+
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  // panic("Not implemented");
+// #endif
+  malloc_reset();
+  size = (size_t)ROUNDUP(size, 8);
+  char *old = hbrk;              // FIXME - hbrk应该是内存指针
+  hbrk += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+    *p = 0;
+  }
+  // Assert((uintptr_t)hbrk - (uintptr_t)heap.start <= setting->mlim);
+  return old;
+  
+  // return NULL;
 }
 
 void free(void *ptr) {
