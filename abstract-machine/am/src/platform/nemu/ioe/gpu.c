96890328f887427d2f1585297338875e1104b854
220031-Miao Heng <1098011272@qq.com>
2024-01-16 22:57:31 +0800

    Debug:VGA将config寄存器中width和height搞反了

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,36 +3,46 @@
 #include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+int width = 0, height = 0;
 
 void __am_gpu_init() {
   int i;
-  int w = inl(VGACTL_ADDR) & 0xFFFF;
-  int h = inl(VGACTL_ADDR) >> 16;
+  height = inl(VGACTL_ADDR) & 0xFFFF;
+  width = inl(VGACTL_ADDR) >> 16;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
+  for (i = 0; i < width * height; i ++) {
+    fb[i] = i;
+  }
   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inl(VGACTL_ADDR) & 0xFFFF, .height = inl(VGACTL_ADDR) >> 16,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  // printf("%d,%d,%d,%d\n", ctl->x, ctl->y, ctl->w, ctl->h);
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  int x_t = 0, y_t = 0, cnt = 0;
+  if(w == 0 || h == 0) return;
+
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;
+  // 从xy为最低点rici
   for(int i = 0; i < h; i ++) {
-    for(int j = 0; j < j; j ++) {
-      
+    for(int j = 0; j < w; j ++) {
+      x_t = x + j;
+      y_t = y + i;
+      fb[y_t * width + x_t] = pixels[cnt++];
+
     }
   }
+
   if (ctl->sync) {
-    // printf("%d,%d,%d,%d,%s\n", ctl->x, ctl->y, ctl->w, ctl->h, ctl->pixels);
-    // printf("%d,%d,%d,%d\n", ctl->x, ctl->y, ctl->w, ctl->h);
-    // printf("%s\n", ctl->pixels);
     outl(SYNC_ADDR, 1);
   }
 }
